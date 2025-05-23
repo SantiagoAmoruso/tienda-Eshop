fccfa716e43ff4f43adb04420d1341ea27a52235
crissgnz-dev <gonzalez.cristian@tecnica7.edu.ar>
2025-05-23 14:47:58 -0300

    modulacion de header y footer / inicio con productos / responsive

--- java/script.js
+++ java/script.js
@@ -1,12 +1,31 @@
 const header = document.querySelector("header")
+const footer = document.querySelector("footer")
 
 header.innerHTML = `
         <div>
-            <i class="fa-solid fa-shop fa-xl"></i>
+            <div class="logo">
+                <img src="img/logo.png" alt="Logo" />
+            </div>
             <h1>Eshop</h1>
         </div>
-        <div>
-            <a href="#"><i class="fa-solid fa-arrow-right-to-bracket fa-xl" style="color: #000"></i></i></a>
-            <a href="#"><i class="fa-solid fa-cart-shopping fa-xl" style="color: #000"></i></a>
+        
+        <input type="checkbox" id="menu" />
+        <label for="menu" class="menu-icon">
+            <span><img src="img/menu.png"></span>
+        </label>
+        <nav class="menu">
+            <ul>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Carrito</a></li>
+            </ul>
+        </nav>
+
+        <div class="opciones">
+            <a href="#"><i class="fa-solid fa-arrow-right-to-bracket fa-xl"></i></i> Login</a>
+            <a href="#"><i class="fa-solid fa-cart-shopping fa-xl"></i> Carrito</a>
         </div>
 `;
+
+footer.innerHTML = `
+        <p>Copyright © 2025 Eshop. All rights reserved.</p>
+        `
