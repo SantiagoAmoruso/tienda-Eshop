98ba834a7b04ab91ed34a1bc25f5b12398ad2894
crissgnz-dev <gonzalez.cristian@tecnica7.edu.ar>
2025-05-30 20:43:06 -0300

    solucion de estilos

--- java/script.js
+++ java/script.js
@@ -16,15 +16,32 @@ header.innerHTML = `
         <nav class="menu">
             <ul>
             <li><a href="login.html">Login</a></li>
-            <li><a href="#">Carrito</a></li>
+            <li><button onclick="toggleCart()">Carrito</button></li>
             </ul>
         </nav>
 
         <div class="opciones">
-            <a href="login.html"><i class="fa-solid fa-arrow-right-to-bracket fa-xl"></i></i> Login</a>
-            <a href="#"><i class="fa-solid fa-cart-shopping fa-xl"></i> Carrito</a>
+            <a href="login.html"class="opcioncitas"><i class="fa-solid fa-arrow-right-to-bracket fa-xl"></i></i> Login</a>
+            <button class="opcioncitas" onclick="toggleCart()"><i class="fa-solid fa-cart-shopping fa-xl"></i> Carrito</button>
         </div>
-`;
+
+        <div id="cart" class="cart-sidebar">
+            <div class="cart-header">
+            <h2>Tu Carrito</h2>
+            <button class="close-btn" onclick="toggleCart()">×</button>
+            </div>
+            <div class="cart-items">
+            <div class="cart-item">
+                <p>Producto 1</p>
+                <p>$10.00</p>
+            </div>
+            <div class="cart-item">
+                <p>Producto 2</p>
+                <p>$15.00</p>
+            </div>
+            </div>
+        </div>
+            `;  
 
 footer.innerHTML = `
         <p>Copyright © 2025 Eshop. All rights reserved.</p>
