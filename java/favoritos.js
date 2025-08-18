995aad8a298e0f7211360e8595a28a7058c75c8c
a <j>
2025-08-18 20:24:25 -0300

    cambios-favoritos

---
+++ java/favoritos.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const contenedorItems = document.querySelector("section.productos")
+
+    const mostrarFavoritos = () => {
+        recuperarFavoritos()
+        // let favoritos = JSON.parse(localStorage.getItem("favoritos")) || []
+        //descargar el contenedor/limpiar
+        contenedorItems.innerHTML = ""
+
+        favoritos.forEach(item => {
+            title = item.title.toUpperCase()
+            description = item.description.toLowerCase()
+            const card = document.createElement("div")
+            card.className = "card"
+            card.innerHTML = `
+                <img
+                    src="${item.img}"
+                    class="card-img-top"
+                    alt="${item.title}"
+                />
+                <div class="card-body">
+                    <h5 class="card-title">${item.title}</h5>
+                    <p class="card-text">${item.desc}</p>
+                    <p class="card-text"><small class="text-body-secondary">${item.price}</small></p>
+                    <div class="actions">
+                        <button class="btn-carrito">Agregar a carrito</button>
+                        <button class="btn-fav" onclick="this.classList.toggle('favorito')">
+                            <i class="fa-regular fa-star fa-xl" style="color: #161616;"></i>
+                        </button>
+                    </div>
+                </div>
+            `
+
+            //Agrego el producto al contenedor
+            contenedorItems.appendChild(card);
+         
+            card.querySelector("#favorito").onclick = () => {
+                 eliminarDeFavoritos(item.id)
+                 mostrarFavoritos()
+            }
+
+            //agregar al carrito
+            card.querySelector("#addCarrito").onclick = () => {
+                const cant = parseInt(card.querySelector('[name="cant"]').value)
+                agregarAlCarrito(item, cant)              
+            }
+           
+
+        });
+    }
+    mostrarFavoritos()
+})
--- java/script.js
+++ java/script.js
@@ -26,7 +26,7 @@ header.innerHTML = `
             <a href="singin.html" class="opcioncitas"><i class="fa-solid fa-arrow-right-to-bracket fa-xl"></i></i> Sign in</a>
             <a href="login.html" class="opcioncitas"><i class="fa-solid fa-user fa-lg"></i></i></i> Login</a>
             <button class="opcioncitas" onclick="toggleCart()"><i class="fa-solid fa-cart-shopping fa-lg"></i> Carrito</button>
-            <button class="opcioncitas"><i class="fa-solid fa-star fa-lg"></i> Favoritos</button>
+            <a href="favoritos.html" class="opcioncitas"><i class="fa-solid fa-star fa-lg"></i> Favoritos</a>
         </div>
 
         <div id="cart" class="cart-sidebar">
